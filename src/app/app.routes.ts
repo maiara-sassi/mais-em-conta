1d297f83384657dd0797b28b75af41b167359c86
maiara <maiara.sassi@gmail.com>
2025-05-30 18:08:25 -0300

    first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {ListaComponent} from './pages/lista/lista.component';
+import {ComparadorComponent} from './pages/comparador/comparador.component';
+import {HomeComponent} from './pages/home/home.component';
+import {OrcamentoComponent} from './pages/orcamento/orcamento.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'comparador', component: ComparadorComponent },
+  { path: 'lista', component: ListaComponent },
+  { path: 'orcamento', component: OrcamentoComponent },
+  { path: '**', redirectTo: '' },
+];
